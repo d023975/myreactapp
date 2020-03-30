aa61f69ae7c06ef2dc3ea4fc2dcbdcb8237c669a
Ruediger Bachmann <ruediger.bachmann@sap.com>
2020-03-30 08:20:48 +0200

    style choicebar, import css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css"
 import ChoiceBar from './components/ChoiceBar';
 import * as serviceWorker from './serviceWorker';
 
