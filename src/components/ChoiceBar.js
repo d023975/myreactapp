6a7e43fa39cce0fc208a4d5ba1e4fe95eb13f2d7
Ruediger Bachmann <ruediger.bachmann@sap.com>
2020-03-30 15:56:21 +0200

    configure properties, formatted

--- src/components/ChoiceBar.js
+++ src/components/ChoiceBar.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React from 'react';
 
-export default function ChoiceBar({title, percent}){
-    return <div className="ChoiceBar">
-        <div className="Progress" style={{'width': percent + '%'}}>
-<div className="ChoiceBarTitle">{title}</div>
-        </div>
-        <div className="ChoiceBarBadge">123</div>
-    </div>;
+export default function ChoiceBar({ title, percent }) {
+	return (
+		<div className="ChoiceBar">
+			<div className="Progress" style={{ width: percent + '%' }}>
+				<div className="ChoiceBarTitle">{title}</div>
+			</div>
+			<div className="ChoiceBarBadge">123</div>
+		</div>
+	);
 }
