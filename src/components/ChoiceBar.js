d886e9cbb933efff6c734449b41e5790972b6b62
Ruediger Bachmann <ruediger.bachmann@sap.com>
2020-04-01 14:26:35 +0200

    eventhandler and setter

--- src/components/ChoiceBar.js
+++ src/components/ChoiceBar.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 export default function ChoiceBar({ title, percent }) {
+	//Create state , 0 is the initial value
+	const [ count, setCount ] = React.useState(0);
+
+	function handleClick(e) {
+		// e is a react event e.nativeEvent is the native DOM-Event
+		setCount(count + 1);
+	}
+
 	return (
-		<div className="ChoiceBar">
+		<div className="ChoiceBar" onClick={handleClick}>
 			<div className="Progress" style={{ width: percent + '%' }}>
 				<div className="ChoiceBarTitle">{title}</div>
 			</div>
-			<div className="ChoiceBarBadge">123</div>
+			<div className="ChoiceBarBadge">{count}</div>
 		</div>
 	);
 }
