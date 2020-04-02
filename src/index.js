1d01de4780b8bb155f3b8ff913f4d0c4c10fb6e7
Ruediger Bachmann <ruediger.bachmann@sap.com>
2020-04-02 16:49:22 +0200

    voting component without choices

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ChoiceBar from './components/ChoiceBar';
+//import ChoiceBar from './components/ChoiceBar';
 import ModalDialog from './components/ModalDialog';
+import VotingComponent from './components/VotingComponent';
 import * as serviceWorker from './serviceWorker';
 
+// Sample data
+const vote = {
+    title: "How is your day?",
+    description: "Tell me: how has your day been so far?",
+    choices: [
+      { id: "choice_1", title: "Good", count: 7 },
+      { id: "choice_2", title: "Bad", count: 12 },
+      { id: "choice_3", title: "Not sure yet", count: 1 }
+    ]
+  };
 
-ReactDOM.render(<ChoiceBar title="JavaScript" percent={57} />, document.getElementById('root'));
+
+
+ReactDOM.render(<VotingComponent vote={vote} />, document.getElementById('root'));
 var title = { subtitle: 'ModalDialog' };
 ReactDOM.render(<ModalDialog title={title} />, document.getElementById('modalMountPoint'));
 // If you want your app to work offline and load faster, you can change
