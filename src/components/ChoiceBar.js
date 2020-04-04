036df28fae43e424ce339266d448a842d5593a81
Ruediger Bachmann <ruediger.bachmann@sap.com>
2020-04-04 18:31:03 +0200

    refactor: pass variables and handler to ChoiceBar

--- src/components/ChoiceBar.js
+++ src/components/ChoiceBar.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import styles from "./ChoiceBar.module.css";
 
-export default function ChoiceBar({ title, percent }) {
-	//Create state , 0 is the initial value
-	const [ count, setCount ] = React.useState(0);
-
-	function handleClick(e) {
-		// e is a react event e.nativeEvent is the native DOM-Event
-		setCount(count + 1);
-	}
+export default function ChoiceBar({ title, percent, count, onClickHandler }) {
 
 	return (
-		<div className={styles.ChoiceBar} onClick={handleClick}>
+		<div className={styles.ChoiceBar} onClick={onClickHandler}>
 			<div className={styles.Progress} style={{ width: percent + '%' }}>
 				<div className={styles.ChoiceBarTitle}>{title}</div>
 			</div>
